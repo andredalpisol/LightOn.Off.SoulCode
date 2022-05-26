24a45270f2359276869a7f32c0146173b37e6c86
andredalpisol <andredalpisol@gmail.com>
2022-05-25 22:55:49 -0300

    alteração style

--- code.js
+++ code.js
@@ -5,11 +5,15 @@ const botao = document.querySelector('#botao')
 botao.addEventListener('click', function () {
 
     if (lampada1.classList.contains('ligada')) {
+        botao.innerText = 'LIGAR'
+        document.body.style.backgroundColor = "black";
         lampada1.classList.remove('ligada')
         lampada1.classList.add('desligada')
         lampada2.classList.remove('desligada')
         lampada2.classList.add('ligada')
     } else {
+        botao.innerText = 'DESLIGAR'
+        document.body.style.backgroundColor = "yellow";
         lampada1.classList.remove('desligada')
         lampada1.classList.add('ligada');
         lampada2.classList.remove('ligada')
